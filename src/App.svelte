651659f7268a5871b09d302c492f86a250cc9b07
Владислав <vladgrom2206.com@gmail.com>
2022-07-02 16:16:27 +0300

    Fix center alignment of Svelte logo

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <main class="font-sans text-center p-4 my-0 mx-auto">
-  <img class="h-64 w-64" src={logo} alt="Svelte Logo" />
+  <img class="h-64 w-64 my-0 mx-auto" src={logo} alt="Svelte Logo" />
   <h1 class="text-[#ff3e00] uppercase text-6xl font-thin leading-tight my-8 mx-auto max-w-xs sm:max-w-none">Hello Typescript!</h1>
 
   <Counter />
