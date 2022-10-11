b5006736f13da8b1e424c9447846f25726959144
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2022-10-11 13:53:09 +0000

    Update template sources
    Add notes about Tailwind layers
    Move all inline Tailwind classes to <style> tag
    Make container fully centered

--- src/App.svelte
+++ src/App.svelte
@@ -3,20 +3,36 @@
     import Counter from './lib/Counter.svelte';
 </script>
 
-<main class="font-sans text-center p-4 my-0 mx-auto">
-    <img class="h-64 w-64 my-0 mx-auto" src={logo} alt="Svelte Logo" />
-    <h1 class="text-[#ff3e00] uppercase text-6xl font-thin leading-tight my-8 mx-auto max-w-xs sm:max-w-none">
-        Hello Typescript!
-    </h1>
+<main>
+    <img src={logo} alt="Svelte Logo" />
+    <h1>Hello Typescript!</h1>
 
     <Counter />
 
-    <p class="max-w-xs sm:max-w-none my-4 mx-auto leading-snug">
+    <p>
         Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.
     </p>
 
-    <p class="max-w-xs sm:max-w-none my-4 mx-auto leading-snug">
+    <p>
         Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for the officially supported framework,
         also powered by Vite!
     </p>
 </main>
+
+<style lang="postcss">
+    main {
+        @apply flex flex-col items-center justify-center font-sans text-center p-4 h-screen mx-auto;
+    }
+
+    img {
+        @apply h-64 w-64 my-0 mx-auto;
+    }
+
+    h1 {
+        @apply text-[#ff3e00] uppercase text-6xl font-thin leading-tight my-8 mx-auto max-w-xs sm:max-w-none;
+    }
+
+    p {
+        @apply max-w-xs sm:max-w-none my-4 mx-auto leading-snug;
+    }
+</style>
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -5,9 +5,12 @@
     };
 </script>
 
-<button
-    class="py-4 px-8 text-[#ff3e00] bg-[#ff3e001a] active:bg-[#ff3e0033] rounded-full border-2 border-solid border-[#ff3e0000] focus:border-[#ff3e00] outline-none w-52 tabular-nums cursor-pointer"
-    on:click={increment}
->
+<button on:click={increment}>
     Clicks: {count}
 </button>
+
+<style lang="postcss">
+    button {
+        @apply py-4 px-8 text-[#ff3e00] bg-[#ff3e001a] active:bg-[#ff3e0033] rounded-full border-2 border-solid border-[#ff3e0000] focus:border-[#ff3e00] outline-none w-52 tabular-nums cursor-pointer;
+    }
+</style>
