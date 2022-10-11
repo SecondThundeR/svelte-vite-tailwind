924346033f109436fa85d76926612cc86fda87dc
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2022-10-11 13:53:09 +0000

    Update template sources
    Add notes about Tailwind layers
    Move all inline Tailwind classes to <style> tag
    Make container fully centered

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
-  let count: number = 0
+  let count: number = 0;
   const increment = () => {
-    count += 1
-  }
+    count += 1;
+  };
 </script>
 
-<button class="py-4 px-8 text-[#ff3e00] bg-[#ff3e001a] active:bg-[#ff3e0033] rounded-full border-2 border-solid border-[#ff3e0000] focus:border-[#ff3e00] outline-none w-52 tabular-nums cursor-pointer" on:click={increment}>
+<button on:click={increment}>
   Clicks: {count}
 </button>
+
+<style lang="postcss">
+  button {
+    @apply py-4 px-8 text-[#ff3e00] bg-[#ff3e001a] active:bg-[#ff3e0033] rounded-full border-2 border-solid border-[#ff3e0000] focus:border-[#ff3e00] outline-none w-52 tabular-nums cursor-pointer;
+  }
+</style>
