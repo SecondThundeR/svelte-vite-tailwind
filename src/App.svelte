c890c3665764d0e88b5de566255a737ae35725f4
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-10 17:43:12 +0300

    feat: sync with latest Vite's template for Svelte
    Styles optimized for Tailwind CSS pixel* perfect
    
    *Subject to proof :0

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,43 @@
 <script lang="ts">
-  import logo from "./assets/svelte.png";
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
   import Counter from "./lib/Counter.svelte";
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
+  <div class="flex gap-4 justify-center">
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
 
-  <Counter />
+  <div class="card">
+    <Counter />
+  </div>
 
   <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
   </p>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style lang="postcss">
-  main {
-    @apply flex flex-col items-center justify-center font-sans text-center p-4 h-screen mx-auto;
+  .logo {
+    @apply h-36 p-[1.5em] will-change-[filter] transition-[filter] duration-300 hover:drop-shadow-[0_0_2em_#646cffaa];
   }
-
-  img {
-    @apply h-64 w-64 my-0 mx-auto;
+  .logo.svelte:hover {
+    @apply drop-shadow-[0_0_2em_#ff3e00aa];
   }
-
-  h1 {
-    @apply text-[#ff3e00] uppercase text-6xl font-thin leading-tight my-8 mx-auto max-w-xs sm:max-w-none;
-  }
-
-  p {
-    @apply max-w-xs sm:max-w-none my-4 mx-auto leading-snug;
+  .read-the-docs {
+    @apply text-[#888];
   }
 </style>
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -6,11 +6,5 @@
 </script>
 
 <button on:click={increment}>
-  Clicks: {count}
+  count is {count}
 </button>
-
-<style lang="postcss">
-  button {
-    @apply py-4 px-8 text-[#ff3e00] bg-[#ff3e001a] active:bg-[#ff3e0033] rounded-full border-2 border-solid border-[#ff3e0000] focus:border-[#ff3e00] outline-none w-52 tabular-nums cursor-pointer;
-  }
-</style>
